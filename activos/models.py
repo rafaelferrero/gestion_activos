c3d264a63c9f7ae9500157eef66b4e42728e861c
Rafael Ferrero <rafael.ferrero@gmail.com>
2017-01-16 11:47:02 -0300

    Más PEP8 y cuando se grabe el movimiento cambia la ubicacion en Activos

--- activos/models.py
+++ activos/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from datetime import datetime
     
-# Create your models here.
+
 class Marcas(models.Model):
-    marca = models.CharField(max_length=200)
+    marca = models.CharField(
+        max_length=200)
     
-    def __unicode__( self ):
+    def __unicode__(self):
         return self.marca
     
     class Meta:
@@ -15,24 +16,31 @@ class Marcas(models.Model):
 
 
 class Tipos(models.Model):
-    tipo = models.CharField(max_length=200)
+    tipo = models.CharField(
+        max_length=200)
     
-    def __unicode__( self ):
+    def __unicode__(self):
         return self.tipo
     
     class Meta:
         verbose_name = 'Tipo'
         verbose_name_plural = 'Tipos'
-        ordering = [ 'tipo' ]
+        ordering = ['tipo']
 
 
 class Modelos(models.Model):
-    modelo = models.CharField(max_length=200)
-    marca = models.ForeignKey(Marcas)
-    tipo = models.ForeignKey(Tipos)
+    modelo = models.CharField(
+        max_length=200)
+    marca = models.ForeignKey(
+        Marcas)
+    tipo = models.ForeignKey(
+        Tipos)
     
-    def __unicode__( self ):
-        return "{0} - {1} - {2}".format(self.tipo, self.marca, self.modelo)
+    def __unicode__(self):
+        return u"{0} - {1} - {2}".format(
+            self.tipo,
+            self.marca,
+            self.modelo)
 
     class Meta:
         verbose_name = 'Modelo'
@@ -41,9 +49,10 @@ class Modelos(models.Model):
 
 
 class Ubicaciones(models.Model):
-    ubicacion = models.CharField(max_length=200)
+    ubicacion = models.CharField(
+        max_length=200)
     
-    def __unicode__( self ):
+    def __unicode__(self):
         return self.ubicacion
 
     class Meta:
@@ -53,26 +62,52 @@ class Ubicaciones(models.Model):
 
 
 class Activos(models.Model):
-    PESOS='$'
-    DOLLAR='u$s'
-    MONEDA_CHOICES=(
-        (PESOS,'$'),
-        (DOLLAR,'u$s'),
+    PESOS = '$'
+    DOLLAR = 'u$s'
+    MONEDA_CHOICES = (
+        (PESOS, '$'),
+        (DOLLAR, 'u$s'),
     )
 
-    codigo=models.CharField('Codigo Interno', max_length=7, unique=True)
-    numero_serie=models.CharField(max_length=200)
-    modelo=models.ForeignKey(Modelos)
-    nro_factura=models.CharField(max_length=50, blank=True, null=True)
-    moneda=models.CharField(max_length=3, choices=MONEDA_CHOICES, default=PESOS)
-    valor=models.DecimalField('Valor Declarado', max_digits=11, decimal_places=2, blank=True, null=True)
-    detalle=models.CharField(max_length=500, blank=True)
-    ubicacion=models.ForeignKey(Ubicaciones, blank=True, null=True)
-    asegurable=models.BooleanField(default=False)
-    asegurado=models.BooleanField(default=False)
+    codigo = models.CharField(
+        'Codigo Interno',
+        max_length=7,
+        unique=True)
+    numero_serie = models.CharField(
+        max_length=200)
+    modelo = models.ForeignKey(Modelos)
+    nro_factura = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True)
+    moneda = models.CharField(
+        max_length=3,
+        choices=MONEDA_CHOICES,
+        default=PESOS)
+    valor = models.DecimalField(
+        'Valor Declarado',
+        max_digits=11,
+        decimal_places=2,
+        blank=True,
+        null=True)
+    detalle = models.CharField(
+        max_length=500,
+        blank=True)
+    ubicacion = models.ForeignKey(
+        Ubicaciones,
+        blank=True,
+        null=True)
+    asegurable = models.BooleanField(
+        default=False)
+    asegurado = models.BooleanField(
+        default=False)
 
-    def __unicode__( self ):
-        return '%s - %s - %s - %s' % (self.codigo, self.modelo, self.numero_serie, self.ubicacion)
+    def __unicode__(self):
+        return u"{0} - {1} - {2} - {3}".format(
+            self.codigo,
+            self.modelo,
+            self.numero_serie,
+            self.ubicacion)
 
     class Meta:
         verbose_name = 'Activo'
@@ -81,15 +116,29 @@ class Activos(models.Model):
 
 
 class Movimientos(models.Model):
-    activo = models.ForeignKey(Activos)
-    origen = models.ForeignKey(Ubicaciones, related_name='ubicacion_origen')
-    destino = models.ForeignKey(Ubicaciones, related_name='ubicacion_destino')
-    fecha = models.DateTimeField(default=datetime.now())
+    activo = models.ForeignKey(
+        Activos)
+    origen = models.ForeignKey(
+        Ubicaciones,
+        related_name='ubicacion_origen')
+    destino = models.ForeignKey(
+        Ubicaciones,
+        related_name='ubicacion_destino')
+    fecha = models.DateTimeField(
+        default=datetime.now())
     
     def __unicode__( self ):
-        #return '%s: de %s a %s el %s' % ( self.activo, self.origen, self.destino, self.fecha )
-        return '%s: de %s a %s' % ( self.activo, self.origen, self.destino )
-    
+        return u"{0}: de {1} a {2}".format(
+            self.activo,
+            self.origen,
+            self.destino)
+
+    def save(self, *args, **kwargs):
+        a = Activos.objects.get(pk=self.activo.pk)
+        a.ubicacion = self.destino
+        a.save(update_fields=['ubicacion'])
+        super(Movimientos, self).save(*args, **kwargs)
+
     class Meta:
         verbose_name = 'Movimiento del Activo'
         verbose_name_plural = 'Movimientos de los Activos'
@@ -97,29 +146,35 @@ class Movimientos(models.Model):
 
 
 class Estados(models.Model):
-    estado=models.CharField(max_length=200)
+    estado=models.CharField(
+        max_length=200)
     
-    def __unicode__( self ):
+    def __unicode__(self):
         return self.estado
 
     class Meta:
         verbose_name = 'Estado'
         verbose_name_plural = 'Estados'
-        ordering = [ 'estado' ]
+        ordering = ['estado']
 
     
 class Incidencias(models.Model):
-    activo=models.ForeignKey(Activos)
-    estado=models.ForeignKey(Estados)
-    fecha=models.DateTimeField(default=datetime.now())
-    descripcion=models.TextField()
+    activo = models.ForeignKey(
+        Activos)
+    estado = models.ForeignKey(
+        Estados)
+    fecha = models.DateTimeField(
+        default=datetime.now())
+    descripcion = models.TextField()
     
-    def __unicode__( self ):
-        #return '%s - %s (%s): %s' % ( self.fecha, self.activo, self.estado, self.descripcion )
-        return '%s (%s): %s' % ( self.activo, self.estado, self.descripcion )
+    def __unicode__(self):
+        return u"{0} ({1}): {2}".format(
+            self.activo,
+            self.estado,
+            self.descripcion)
     
     class Meta:
         verbose_name = 'Incidencia del Activo'
         verbose_name_plural = 'Incidencias de los Activos'
-        ordering = [ '-fecha', 'activo', 'estado' ]
+        ordering = ['-fecha', 'activo', 'estado']
 
