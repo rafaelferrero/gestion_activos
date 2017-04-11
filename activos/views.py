099d6feb52920884583e4acba1e860a686d1ed9d
Rafael Ferrero <rafael.ferrero@gmail.com>
2017-04-11 18:51:23 -0300

    corrijo el view y las urls

--- activos/views.py
+++ activos/views.py
@@ -40,57 +40,73 @@ def getResumenGral(request):
 
 
 def getResumenSeg(request, cmd=''):
-    plantilla = 'seguro.html'
-    activos = Activos.objects.all().select_related().order_by('codigo')
     stPesos = {}
     stDolar = {}
     tPesos = 0.0
     tDolar = 0.0
+    if cmd is None:
+        activos = []
+        registro = []
+        plantilla = 'resumen.html'
+        activos_t = Activos.objects.select_related()
+        for activo in activos_t:
+            registro.append(activo)
+            try:
+                movimientos = Movimientos.objects.select_related().filter(
+                    activo=activo.id)
+                registro.append(movimientos)
+            except ObjectDoesNotExist:
+                pass
+            activos.append(registro)
+            registro = []
+    else:
+        plantilla = 'seguro.html'
+        activos = Activos.objects.all().select_related().order_by('codigo')
 
-    if cmd == 'asegurables':
-        activos = Activos.objects.select_related()
-        activos = activos.exclude(ubicacion__isnull=True)
-        activos = activos.filter(asegurable=True)
-        activos = activos.order_by('ubicacion',  'codigo')
-    elif cmd == 'asegurados':
-        activos = Activos.objects.select_related()
-        activos = activos.exclude(ubicacion__isnull=True)
-        activos = activos.filter(asegurable=True)
-        activos = activos.filter(asegurado=True)
-        activos = activos.order_by('codigo')
-    elif cmd == 'noasegurados':
-        activos = Activos.objects.select_related()
-        activos = activos.exclude(ubicacion__isnull=True)
-        activos = activos.filter(asegurable=True)
-        activos = activos.filter(asegurado=False)
-        activos = activos.order_by('codigo')
-    elif cmd == 'sinvalorizar':
-        activos = Activos.objects.select_related()
-        activos = activos.exclude(ubicacion__isnull=True)
-        activos = activos.filter(asegurable=True)
-        activos = activos.filter(valor=0)
-        activos = activos.order_by('codigo')
-    elif cmd == 'asegurar':
-        activos = Activos.objects.select_related()
-        activos = activos.exclude(ubicacion__isnull=True)
-        activos = activos.filter(asegurable=True)
-        activos = activos.exclude(ubicacion__ubicacion__icontains='ALMACEN')
-        activos = activos.order_by('codigo')
-        
-    for activo in activos:
-        if activo.moneda == '$':
-            tPesos += float(activo.valor)
-            if stPesos.has_key(activo.modelo.tipo):
-                stPesos[activo.modelo.tipo] = stPesos.get(activo.modelo.tipo) + float(activo.valor)
-            else:
-                stPesos[activo.modelo.tipo] = float(activo.valor)
-        elif activo.moneda == 'u$s':
-            tDolar += float(activo.valor)
-            if stDolar.has_key(activo.modelo.tipo):
-                stDolar[activo.modelo.tipo] = stDolar.get(activo.modelo.tipo) + float(activo.valor)
-            else:
-                stDolar[activo.modelo.tipo] = float(activo.valor)
-                
+        if cmd == 'asegurables':
+            activos = Activos.objects.select_related()
+            activos = activos.exclude(ubicacion__isnull=True)
+            activos = activos.filter(asegurable=True)
+            activos = activos.order_by('ubicacion',  'codigo')
+        elif cmd == 'asegurados':
+            activos = Activos.objects.select_related()
+            activos = activos.exclude(ubicacion__isnull=True)
+            activos = activos.filter(asegurable=True)
+            activos = activos.filter(asegurado=True)
+            activos = activos.order_by('codigo')
+        elif cmd == 'noasegurados':
+            activos = Activos.objects.select_related()
+            activos = activos.exclude(ubicacion__isnull=True)
+            activos = activos.filter(asegurable=True)
+            activos = activos.filter(asegurado=False)
+            activos = activos.order_by('codigo')
+        elif cmd == 'sinvalorizar':
+            activos = Activos.objects.select_related()
+            activos = activos.exclude(ubicacion__isnull=True)
+            activos = activos.filter(asegurable=True)
+            activos = activos.filter(valor=0)
+            activos = activos.order_by('codigo')
+        elif cmd == 'asegurar':
+            activos = Activos.objects.select_related()
+            activos = activos.exclude(ubicacion__isnull=True)
+            activos = activos.filter(asegurable=True)
+            activos = activos.exclude(ubicacion__ubicacion__icontains='ALMACEN')
+            activos = activos.order_by('codigo')
+
+        for activo in activos:
+            if activo.moneda == '$':
+                tPesos += float(activo.valor)
+                if stPesos.has_key(activo.modelo.tipo):
+                    stPesos[activo.modelo.tipo] = stPesos.get(activo.modelo.tipo) + float(activo.valor)
+                else:
+                    stPesos[activo.modelo.tipo] = float(activo.valor)
+            elif activo.moneda == 'u$s':
+                tDolar += float(activo.valor)
+                if stDolar.has_key(activo.modelo.tipo):
+                    stDolar[activo.modelo.tipo] = stDolar.get(activo.modelo.tipo) + float(activo.valor)
+                else:
+                    stDolar[activo.modelo.tipo] = float(activo.valor)
+
     return render_to_response(
         plantilla,
         {
@@ -99,7 +115,7 @@ def getResumenSeg(request, cmd=''):
             'stDolar': stDolar,
             'tPesos': tPesos,
             'tDolar': tDolar,
-         },
+        },
         context_instance=RequestContext(request)
     )
 
--- gestion_activos/urls.py
+++ gestion_activos/urls.py
@@ -2,39 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from activos import views
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^grappelli/',
-        include('grappelli.urls')),  # grappelli URLS
-
-    url(r'^admin/',
-        include(admin.site.urls)),  # admin URLS
     url(r'^$', 'activos.views.index', name='index'),
-    url(r'^resumen/todos',
-        'activos.views.getResumenGral'),
-    url(r'^resumen/noasegurables$',
-        'activos.views.getResumenSeg',
-        {'cmd': 'noasegurables$'}),
-    url(r'^resumen/seguro/todos$',
-        'activos.views.getResumenSeg'),
-    url(r'^resumen/seguro/asegurables',
-        'activos.views.getResumenSeg',
-        {'cmd': 'asegurables'}),
-    url(r'^resumen/seguro/asegurados',
-        'activos.views.getResumenSeg',
-        {'cmd': 'asegurados'}),
-    url(r'^resumen/seguro/noasegurados$',
-        'activos.views.getResumenSeg',
-        {'cmd': 'noasegurados'}),
-    url(r'^resumen/seguro/sinvalorizar',
-        'activos.views.getResumenSeg',
-        {'cmd': 'sinvalorizar'}),
-    url(r'^resumen/seguro/asegurar',
-        'activos.views.getResumenSeg',
-        {'cmd': 'asegurar'}),
-    url(r'^tmp/',
-        'activos.views.tmp',)
+    url(r'^admin/', include(admin.site.urls)),  # admin URLS
+    url(r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
+    url(r'^resumen/(?:(?P<cmd>\w+)/)?$', views.getResumenSeg, name="resumen"),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
